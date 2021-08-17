e78ee5e35c8ff7e8b17be8b1f9216f12284f6bac
Tabita <tabita.barbosa8@gmail.com>
2021-08-17 00:50:41 -0300

    adiciona sons e delay nos botões

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,17 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
     @IBAction func keyPressed(_ sender: UIButton) {
-        print("fui clicado")
+        playSound(soundName: sender.currentTitle!)
+        
+        sender.alpha = 0.3
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5){
+            sender.alpha = 1.0
+        }
+        
+    }
+    
+    func playSound(soundName: String){
+        
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
     }
     
 }
